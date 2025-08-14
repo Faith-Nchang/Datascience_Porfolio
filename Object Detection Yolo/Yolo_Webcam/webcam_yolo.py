e207653a3f5d1c29694a03ee92f1d7f8f38d8063
Faith Nchang <143855102+Faith-Nchang@users.noreply.github.com>
2025-08-14 14:07:54 -0400

    perform ETA on dataset

--- Object Detection Yolo/Yolo_Webcam/webcam_yolo.py
+++ Object Detection Yolo/Yolo_Webcam/webcam_yolo.py
@@ -4,10 +4,9 @@ import cvzone
 import math
 import time
 
-cap = cv2.VideoCapture(1)  # For Webcam
+cap = cv2.VideoCapture(0)  # For Webcam
 cap.set(3, 1280)
 cap.set(4, 720)
-# cap = cv2.VideoCapture("../Videos/motorbikes.mp4")  # For Video
 
 
 model = YOLO("../Yolo-Weights/yolov8l.pt")
